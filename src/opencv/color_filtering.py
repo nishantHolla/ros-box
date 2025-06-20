e645adb6f4c1e5ef2dba7c9c92be3359c8a99680
Nishant Holla <nishantholla19@gmail.com>
2025-06-20 19:26:03 +0530

    Add color filtering in opencv

---
+++ src/opencv/color_filtering.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import numpy as np
+import cv2
+
+image = cv2.imread("./images/tree.jpg")
+cv2.imshow("Original", image)
+
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+cv2.imshow("HSV", hsv)
+
+'''
+red: 0 to 30
+yellow: 30 to 60
+green: 60 to 90
+cyan: 90 to 120
+blue: 120 to 150
+magenta: 150 to 180
+'''
+
+green = (
+    ( 40, 0, 0 ), # Lower bound H, S, V
+    ( 120, 255, 255) # Upper bound H, S, V
+)
+
+# Define a mask
+mask = cv2.inRange(image, green[0], green[1])
+
+cv2.imshow("Color filter", mask)
+cv2.waitKey(0)
+
+cv2.waitKey(0)
